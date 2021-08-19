916a365b6f26709dbe8b2dfc2604504d87aaa04f
MarkC-b3d <blendedmarks@gmail.com>
2021-08-19 01:33:59 +0100

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,47 @@
+from rich import print
+from rich.panel import Panel
+from rich.console import Console
+from rich.text import Text
+import os
+from os import system, name
+import subprocess
+
+
+def init():
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+        deploy = Panel(Text("Deploy Silicon Render Node v1.0", style="bold green", justify="Left"))
+        prod_panel = Panel(Text("<===================| EXPERIMENTAL BUILD |===================>", style="bold red", justify="center"))
+        print(deploy)
+        print(prod_panel)
+        dir_check()
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+        _ = system('cls')
+        deploy = Panel(Text("Deploy Silicon Render Node v1.0", style="bold green", justify="Left"))
+        prod_panel = Panel(Text("<===================| EXPERIMENTAL BUILD |===================>", style="bold red", justify="center"))
+        print(deploy)
+        print(prod_panel)
+        dir_check()
+
+
+def dir_check():
+    if os.path.isdir('./silicon'):
+        console = Console()
+        text = Text("Silicon Directory Exists...skipping")
+        text.stylize("bold blue")
+        console.print(text)
+    else:
+        git_clone()
+
+
+def git_clone():
+    console = Console()
+    text = Text("Downloading Silicon....")
+    text.stylize("bold blue")
+    subprocess.Popen("git clone https://github.com/MarkC-b3d/silicon")
+    console.print(text)
+    
+init()
